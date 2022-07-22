9fc0bba7e870a002d4466c93288431f5582676e2
TuTioElPollo <gamerbloom5@gmail.com>
2022-07-21 21:29:24 -0600

    Upload 10

--- 5. Propiedades y Metodos de los tipos de dato/Propiedades de Datos Int/Propiedades de Datos Int/Program.cs
+++ 5. Propiedades y Metodos de los tipos de dato/Propiedades de Datos Int/Propiedades de Datos Int/Program.cs
@@ -63,6 +63,88 @@ namespace Propiedades_de_Datos_Int
             Console.WriteLine(Math.Abs(d));                 // Math.Abs() regresa el valor absoluto de una variable
 
 
+                            // ----------------------- IndexOf, Find y FindAll  ----------------------- //
+
+            int[] numeros = { 5, 18, 21, 4, 13 };
+            //    Indices     0   1   2  3   4
+            string[] nombres = { "Pedro", "Luis", "Jose", "Felipe" };
+            int Longitud = numeros.Length;
+
+            
+            int Primer = Array.Find(numeros, p => p > 15);          // .Find extrae de un array el primer elemento que cumpla la condicion
+                                                                    // p es la variable de rango que recorre el array y luego de => es la condicion
+
+            int[] todos = Array.FindAll(numeros, p => p > 15);      // .FindAll extrae todos los elementos que cumplan la condicion
+                                                                    // p es la variable de rango que recorre el array y luego de => es la condicion
+
+            int indice = Array.IndexOf(numeros, 20);                // .IndexOf solo busa en el array si existe la condición y si no regresa valor -1
+            if(indice > 0)
+            {
+                Console.WriteLine("Existe el valor");
+            }
+            else
+            {
+                Console.WriteLine("No existe");
+            }
+
+            int ultimo = Array.FindLast(numeros, p => p > 15);      // .FindLast extrae de un array el ultimo elemento que cumpla la condicion
+                                                                    // p es la variable de rango que recorre el array y luego de => es la condicion
+
+            int Indice = Array.FindIndex(numeros, p => p > 10);     // .FindIndex extrae el indice de donde se cumple la primera condicion en el array
+
+            int Ultimo = Array.FindLastIndex(numeros, p => p > 15); // .FindLastIndex extrae el indice de donde se cumple la ultima condicion
+
+            bool Verf = Array.Exists(nombres, p => p.StartsWith("J"));
+            if (exito)
+            {
+                Console.WriteLine("Hay elementos que cumplen");     // .Exist regresa un true si hay elementos que cumplen la condicion y false si no
+            }
+
+
+            // ------------------------------ TimeSpan  ------------------------------ //
+
+            TimeSpan oTimeSpan = new TimeSpan(4, 10, 29);           // TimeSpan genera un tipo de dato de tiempo
+            TimeSpan oTimeSpan2 = new TimeSpan(5, 15, 10);          // TimeSpan time1 = new TimeSpan(1,0,0,0);
+                                                                    // TimeSpan time2 = new TimeSpan(13,0,0);
+            TimeSpan oTimeSpan3 = oTimeSpan + oTimeSpan2;
+            Console.WriteLine(oTimeSpan3);                          // Regresa 09, 25, 39    (Horas, Minutos, Segundos)
+            TimeSpan oTimeSpan4 = oTimeSpan2 - oTimeSpan;
+            Console.WriteLine(oTimeSpan4);                          // Regresa 01, 04, 41    (Horas, Minutos, Segundos)
+
+            TimeSpan objeto1 = new TimeSpan(5, 4, 3, 2, 1);         // ( Dias, Horas, Minutos, Segundos, Milisegundos )
+
+            // Propiedades
+            Console.WriteLine(objeto1.Days);                        // .Days obtiene los dias de un TimeSpan
+            Console.WriteLine(objeto1.Hours);                       // .Hours obtiene las horas de un TimeSpan
+            Console.WriteLine(objeto1.Minutes);                     // .Minutes obtiene los minutos de un TimeSpan
+            Console.WriteLine(objeto1.Seconds);                     // .Seconds obtiene los segundos de un TimeSpan
+            Console.WriteLine(objeto1.Milliseconds);                // .Miliseconds obtiene los milisegundos de un TimeSpan
+
+            // Metodos
+            TimeSpan fecha1 = new TimeSpan(15, 12, 15, 15, 15);
+            TimeSpan fecha2 = new TimeSpan(5, 48, 15, 45, 45);
+            TimeSpan fecha3 = fecha1 + fecha2;
+
+            Console.WriteLine(fecha3.TotalDays);                    // .TotalDays convierte la fecha a los dias totales que contiene el TimeSpan
+            Console.WriteLine(fecha3.TotalHours);                   // .TotalHours convierte la fecha a las horas totales que contiene el TimeSpan
+            Console.WriteLine(fecha3.TotalMinutes);                 // .TotalMinutes convierte la fecha a los minutos totales que contiene el TimeSpan
+            Console.WriteLine(fecha3.TotalSeconds);                 // .TotalSeconds convierte la fecha a los segundos totales que contiene el TimeSpan
+            Console.WriteLine(fecha3.TotalMilliseconds);            // .TotalMiliseconds convierte la fecha a los milisegundos totales que contiene el TimeSpan
+
+            TimeSpan fecha4 = fecha2.Add(fecha1);                   // .Add cumple la funcion de (fecha1 + fecha2)
+
+            Console.WriteLine(fecha2.CompareTo(fecha1));            // .CompareTo si fecha1 es mayor regresa -1 y si fecha1 es menor regresa 1 como valor
+
+            Console.WriteLine(TimeSpan.FromDays(1));                // .FromDays convierte el valor a un TimeSpan de 01: 00: 00: 000
+            Console.WriteLine(TimeSpan.FromHours(2));               // .FromHours convierte el valor a un TimeSpan de 02: 00: 00
+            Console.WriteLine(TimeSpan.FromMinutes(3));             // .FromMinutes convierte el valor a un TimeSpan de 00: 03: 00
+            Console.WriteLine(TimeSpan.FromSeconds(4));             // .FromSeconds convierte el valor a un TimeSpan de 00: 00: 04
+            Console.WriteLine(TimeSpan.FromMilliseconds(5));        // .FromMiliseconds convierte el valor a un TimeSpan de 00: 00: 00.0080000
+
+            string Fecha = "17:20:10";
+            TimeSpan.ParseExact(Fecha, @"h\:mm\:ss", null);         // .ParseExact convierte una cadena a un formato de TimeSpan
+                                                                    // formato ( variable, @"h\:mm\:ss" , null)
+
 
             Console.ReadKey();
         }
