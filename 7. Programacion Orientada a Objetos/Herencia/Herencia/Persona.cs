af0fe1252bb56fb9a08528ba0ea71654fecb9be5
DavidAmaya-TTEP <gamerbloom5@gmail.com>
2022-08-13 14:49:30 -0600

    Upload 18

---
+++ 7. Programacion Orientada a Objetos/Herencia/Herencia/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    // Superclase (de esta van a heredar los hijos las propiedades y metodos) (se hace para que las clases eviten repetir las mismas propiedades)
+    public class Persona
+    {
+        public string nombre { get; set; }
+        public string apPaterno { get; set; }
+        public string apMaterno { get; set; }
+        public int edad { get; set; }
+
+        public string nombreCompleto()
+        {
+            return this.nombre + " " + this.apPaterno + " " + this.apMaterno;
+        }
+    }
+}
