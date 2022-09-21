386ca26bf17f738e0351740ffbf4c64d2e2cc20f
DavidAmaya-TTEP <gamerbloom5@gmail.com>
2022-09-20 21:00:54 -0600

    Upload 28

--- 10. Propiedades de los controles/Creacion y propiedades de formulario/Formulario Intro/Formulario Intro/Form1.Designer.cs
+++ 10. Propiedades de los controles/Creacion y propiedades de formulario/Formulario Intro/Formulario Intro/Form1.Designer.cs
@@ -31,13 +31,20 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.txtcampo = new System.Windows.Forms.TextBox();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
-            this.label1.Location = new System.Drawing.Point(145, 70);
+            this.label1.Location = new System.Drawing.Point(499, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(446, 336);
             this.label1.TabIndex = 0;
@@ -47,12 +54,73 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(121, 446);
+            this.label2.Location = new System.Drawing.Point(58, 47);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(143, 21);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Ingrese numero 2";
+            this.label2.Text = "Ingrese numero 1";
             this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtcampo
+            // 
+            this.txtcampo.Location = new System.Drawing.Point(58, 103);
+            this.txtcampo.MaxLength = 50;
+            this.txtcampo.Multiline = true;
+            this.txtcampo.Name = "txtcampo";
+            this.txtcampo.Size = new System.Drawing.Size(290, 65);
+            this.txtcampo.TabIndex = 2;
+            this.txtcampo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnguardar.Location = new System.Drawing.Point(58, 333);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(108, 23);
+            this.btnguardar.TabIndex = 3;
+            this.btnguardar.Text = "Guardar";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Lunes",
+            "Martes",
+            "Miercoles",
+            "Jueves",
+            "Viernes",
+            "Sabado",
+            "Domingo"});
+            this.comboBox1.Location = new System.Drawing.Point(58, 190);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(290, 23);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(58, 228);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(290, 23);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(58, 268);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(290, 23);
+            this.dateTimePicker1.TabIndex = 6;
             // 
             // FormPrincipal
             // 
@@ -60,6 +128,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(982, 565);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.txtcampo);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
@@ -69,6 +142,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Nuestro primer formulario";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,5 +152,10 @@
 
         private Label label1;
         private Label label2;
+        private TextBox txtcampo;
+        private Button btnguardar;
+        private ComboBox comboBox1;
+        private NumericUpDown numericUpDown1;
+        private DateTimePicker dateTimePicker1;
     }
 }
